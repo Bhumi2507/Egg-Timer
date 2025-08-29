63138731169c4b39132ac9fc18155c7582d39119
Bhumi Singh <singhbhumi2507@gmail.com>
2025-08-29 14:54:33 +0530

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,19 @@
+let totalSeconds = 539;
+const timerElement = document.getElementById("timer");
+const alarmSound = document.getElementById("alarmSound");
+const countdown = setInterval(() => {
+    if (totalSeconds <= 0) {
+        clearInterval(countdown);
+        timerElement.innerText = "Time's up!";
+        alarmSound.play();
+    } else {
+        const hours = Math.floor(totalSeconds / 3600);
+        const minutes = Math.floor((totalSeconds % 3600) / 60);
+        const seconds = totalSeconds % 60;
+        timerElement.innerText = 
+        String(hours).padStart(2, '0') + ":" +
+        String(minutes).padStart(2, '0') + ":" +
+        String(seconds).padStart(2, '0');
+        totalSeconds--;
+    }
+}, 1000);
